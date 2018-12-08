b1a9ec6e273047fb42f8dee053a8273d7ab2ab9c
Gabe Levasseur <gabel0287@gmail.com>
2018-12-08 15:32:26 -0800

    add elastic output format

---
+++ src/formats/elastic.js
@@ -0,0 +1,20 @@
+/*globals exports, JSON */
+
+'use strict';
+
+exports.format = format;
+
+function format (result) {
+    function replacer(key, value) {
+      if (typeof value === "boolean" || key === 'identifiers') {
+          return String(value);
+        }
+      return value;
+    }
+
+    let formattedResult = '';
+    result.reports.forEach( (r,index) => formattedResult += JSON.stringify({index: {_id: index}}) +'\n' + JSON.stringify(r, replacer) + '\n')
+
+    return formattedResult;
+}
+

c6988274c803410edee60480590a7480b57328a9
Gabe Levasseur <gabel0287@gmail.com>
2018-12-28 22:32:48 -0800

    wip: create markdown report

--- src/formats/markdown.js
+++ src/formats/markdown.js
@@ -5,23 +5,63 @@
 exports.format = format;
 
 function format (result) {
+    /*
     return result.reports.reduce(function (formatted, report) {
         return formatted + formatModule(report) + '\n\n';
     }, formatProject(result));
+    */
+    return formatProject(result) + '\n\n' +
+        '### Ten Functions with Highest Complexity \n\n' +
+        createTableForFunctions(mostComplexFunctions(result)) + '\n\n' +
+        '### Ten Longest Functions \n\n' +
+        createTableForFunctionLengths(longestFunctions(result))
 }
 
 function formatProject (result) {
     return [
-        '# Complexity report, ', (new Date()).toLocaleDateString(), '\n\n',
-        '* Mean per-function logical LOC: ', result.loc, '\n',
-        '* Mean per-function parameter count: ', result.params, '\n',
-        '* Mean per-function cyclomatic complexity: ', result.cyclomatic, '\n',
-        '* Mean per-function Halstead effort: ', result.effort, '\n',
-        '* Mean per-module maintainability index: ', result.maintainability, '\n',
-        '* First-order density: ', result.firstOrderDensity, '%\n',
-        '* Change cost: ', result.changeCost, '%\n',
-        '* Core size: ', result.coreSize, '%\n\n'
+        '# CodeSherpas Complexity Report\n\n',
+        'This repository has an average cyclomatic complexity of **', result.moduleAverage.methodAverage.cyclomatic, '** ',
+        'and a maintainability index of **', result.moduleAverage.maintainability, '**.\n',
+        'Each function has an average of ', result.moduleAverage.methodAverage.sloc.logical, ' lines of code and ',
+        result.moduleAverage.methodAverage.paramCount, ' parameters.\n',
+        'The Halstead Metrics are as follows: \n',
+        '* Effort: ', result.moduleAverage.methodAverage.halstead.effort, '\n',
+        '* Bugs: ', result.moduleAverage.methodAverage.halstead.bugs, '\n',
+        '* Difficulty: ', result.moduleAverage.methodAverage.halstead.difficulty, '\n',
+        '* Time: ', result.moduleAverage.methodAverage.halstead.time, '\n',
+        '* Volume: ', result.moduleAverage.methodAverage.halstead.volume, '\n',
     ].join('');
+}
+
+function createTableForFunctions(anArrayOfFunctionReports) {
+    return '|name|complexity|path|line|\n| --- | --- | --- | --- |\n' +
+        anArrayOfFunctionReports.map(r => `|${r.name.replace(/<|>/g,'')}|${r.cyclomatic}|${r.srcPath}|${r.lineStart}|`).join('\n')
+}
+
+function createTableForFunctionLengths(anArrayOfFunctionReports) {
+    return '|name|length|path|line|\n| --- | --- | --- | --- |\n' +
+        anArrayOfFunctionReports.map(r => `|${r.name.replace(/<|>/g,'')}|${r.lineEnd - r.lineStart}|${r.srcPath}|${r.lineStart}|`).join('\n')
+}
+
+
+function getSortedFunctionReports (aComplexityReport, sortFunction) {
+    return aggregateFunctionReports(aComplexityReport).sort(sortFunction)
+}
+function mostComplexFunctions(aComplexityReport, size = 10) {
+    return getSortedFunctionReports(aComplexityReport, (a,b) => b.cyclomatic - a.cyclomatic).slice(0,size)
+}
+
+function longestFunctions(aComplexityReport, size = 10) {
+    return getSortedFunctionReports(aComplexityReport, (a,b) => (b.lineEnd - b.lineStart) - (a.lineEnd - a.lineStart)).slice(0,size)
+}
+
+function aggregateFunctionReports(aComplexityReport) {
+    const result = [];
+    aComplexityReport.modules.forEach(m => m.methods.forEach(me => {
+        me.srcPath = m.srcPath
+        result.push(me)}
+    ))
+    return result;
 }
 
 function formatModule (report) {
